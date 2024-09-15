002a0b2df98c801a11d8bc35aff8810c783b3168
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2024-09-15 12:19:23 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ApiController],
+  controllers: [ApiController, ApiController],
   providers: [AppService],
 })
 export class AppModule {}
